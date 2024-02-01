8978af948fde4d4b37683f1bfcf9fc6e1817adf1
tchami <131756888+tchamianest@users.noreply.github.com>
2024-02-01 17:53:30 +0200

    End of blog Create dashboard
    i end to listen on click and add new blog to the blog list
    
    Co-Authored-By: Eli Hirwa <hirwaeli@outlook.com>

--- javascript/rich/controlpost.js
+++ javascript/rich/controlpost.js
@@ -1,6 +1,9 @@
 const editor = document.getElementById("richtext");
 const button_post = document.querySelector(".post-blog");
 
+///SELECT BLOG CONATAIRNER
+const blog_dash_store = document.querySelector(".blog-update");
+
 ////// blog container
 const blogs = [
   {
@@ -31,14 +34,45 @@ const blogs = [
 
 // /////////////////////////////////FUNCTION FOR DISPLAY BLOG
 const display = function () {
+  blog_dash_store.innerHTML = "";
   blogs.forEach((blog) => {
     const title = blog.title;
     const image_src = blog.image_src;
     const description = blog.description;
     const smalldescription = blog.small_description;
+
+    ///////TEMPLATE FOR DAHBORD CONTROL
+    const dashBlogtemp = `<div class="text-not">
+    <p>${title}</p>
+    <button class="accept">update</button>
+    <button class="delete">delete</button>
+  </div>`;
+
+    blog_dash_store.insertAdjacentHTML("afterbegin", dashBlogtemp);
   });
 };
+display();
 
 button_post.addEventListener("click", function (events) {
-  console.log(editor.value);
+  const innertext = editor.value;
+  const parser = new DOMParser();
+  const dataparse = parser.parseFromString(innertext, "text/html");
+
+  console.log(dataparse.body);
+  const data_contained = dataparse.body.children;
+
+  //   Array.from(data_contained).forEach((el) => {
+  //     console.log(el.textContent);
+  //   });
+  const newtitle = data_contained[0].innerHTML;
+  const newobj = {
+    title: newtitle,
+    description: "",
+
+    image_src: "",
+    small_description: "",
+  };
+  blogs.push(newobj);
+  console.log(data_contained[0].textContent);
+  display();
 });
